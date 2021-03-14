0f7b49d9071704b51f834bc02221befc1c39bcf0
Daniel Pescador <df.pescador@gmail.com>
2021-03-14 10:51:25 -0500

    Make the API populate the DB.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,6 @@
 class Character < ApplicationRecord
   has_and_belongs_to_many :films
-  belongs_to :race
+  belongs_to :race, optional: true
   belongs_to :planet
 
   validates :name, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,21 @@ require 'net/http'
 require 'json'
 require 'pp'
 
-Film.destroy_all
-Character.destroy_all
-Planet.destroy_all
-Race.destroy_all
+def destroy_everything
+  Film.destroy_all
+  Character.destroy_all
+  Planet.destroy_all
+  Race.destroy_all
+
+  empty_planet = Planet.create(name:'Unknown')
+  empty_race = Race.create(name:'Unknown', planet:empty_planet)
+
+end
+
+def replace_to_htmls(convert)
+  convert.sub! 'http', 'https' unless convert.include? 'https'
+  convert
+end
 
 def get_data_url(url)
   uri = URI(url)
@@ -21,11 +32,9 @@ def get_data_url(url)
   json_res = JSON.parse(res)
 end
 
-def main_data_retrieval()
-
+def retrieve_films_and_planets
   films_url = 'https://swapi.dev/api/films/'
   all_films = get_data_url(films_url)
-  # pp all_films['results'][0]
 
   all_films['results'].each do |single_movie|
     film_created = Film.find_or_create_by(
@@ -38,9 +47,7 @@ def main_data_retrieval()
       url: single_movie['url']
     )
 
-    puts single_movie['title']
-
-    # PLANETS
+    # ADD ALL PLANETS ASSOCCIATED WITH A MOVIE
     single_movie['planets'].each do |single_planet|
       # Convert to HTTPS that works fine
       single_planet.sub! 'http', 'https'
@@ -63,34 +70,174 @@ def main_data_retrieval()
         url: api_planet_url
       )
 
-      # single_movie['characters'].each do |single_character|
-      #   # Convert to HTTPS that works fine
-      #   single_character.sub! 'http', 'https'
-
-      #   # Retrieve all characters assocciated with the film
-      #   api_characters = get_data_url(single_character)
-      #   api_characters_url = api_characters['url'].sub! 'http', 'https'
-
-      #   # Create the objects
-      #   character_create = film_created.characters.find_or_create_by(
-        #     name: api_characters['name'],
-        #     birth_year: api_characters['birth_year'],
-        #     eye_color: api_characters['eye_color'],
-        #     gender: api_characters['gender'],
-        #     hair_color: api_characters['hair_color'],
-        #     height: api_characters['height'],
-        #     skin_color: api_characters['skin_color'],
-        #     url: api_characters_url
-        #   )
-        # end
-
     end
+
+    single_movie['species'].each do |single_race|
+      # Convert to HTTPS that works fine
+      single_race.sub! 'http', 'https'
+
+      # Retrieve all races assocciated with the film
+      api_races = get_data_url(single_race)
+      api_races_url = api_races['url'].sub! 'http', 'https'
+
+      # #retrieve the planet from teh API and match it to the DB
+      race_planet = Planet.new
+      if !api_races['homeworld'].nil?
+        race_planet_url = api_races['homeworld'].sub! 'http', 'https'
+        race_planet_data = get_data_url(race_planet_url)
+        race_planet_data_url = race_planet_data['url'].sub! 'http', 'https'
+        race_planet = Planet.find_or_create_by(
+          name: race_planet_data['name'],
+          climate: race_planet_data['climate'],
+          diameter: race_planet_data['diameter'],
+          gravity: race_planet_data['gravity'],
+          orbital_period: race_planet_data['orbital_period'],
+          population: race_planet_data['population'],
+          rotation_period: race_planet_data['rotation_period'],
+          surface_water: race_planet_data['surface_water'],
+          terrain: race_planet_data['terrain'],
+          url: race_planet_data_url
+        )
+        race_create = film_created.races.find_or_create_by(
+          name: api_races['name'],
+          average_lifespan: api_races['average_lifespan'],
+          classification: api_races['classification'],
+          designation: api_races['designation'],
+          language: api_races['language'],
+          url: api_races_url,
+          planet: race_planet
+        )
+      else
+        race_planet = Planet.where(name: 'Unknown')[0]
+
+      end
+      race_create = film_created.races.find_or_create_by(
+        name: api_races['name'],
+        average_lifespan: api_races['average_lifespan'],
+        classification: api_races['classification'],
+        designation: api_races['designation'],
+        language: api_races['language'],
+        url: api_races_url,
+        planet: race_planet
+        )
+    end
+
+    # RETRIEVE ALL CHARACTERS
+    single_movie['characters'].each do |single_character|
+      # Convert to HTTPS that works fine
+      single_character.sub! 'http', 'https'
+
+      # Retrieve all characters assocciated with the film
+      api_characters = get_data_url(single_character)
+      api_characters_url = replace_to_htmls(api_characters['url'])
+
+      # retrieve the CHARACTER'S PLANET from the API and match it to the DB
+      character_planet = Planet.new
+      if api_characters['homeworld'].present?
+        character_planet_url = replace_to_htmls(api_characters['homeworld'])
+        character_planet_data = get_data_url(character_planet_url)
+        character_planet_data_url = replace_to_htmls(character_planet_data['url'])
+
+        find_planet = Planet.where(name: character_planet_data['name'])
+        if find_planet.length == 0
+          character_planet = Planet.find_or_create_by(
+            name: character_planet_data['name'],
+            climate: character_planet_data['climate'],
+            diameter: character_planet_data['diameter'],
+            gravity: character_planet_data['gravity'],
+            orbital_period: character_planet_data['orbital_period'],
+            population: character_planet_data['population'],
+            rotation_period: character_planet_data['rotation_period'],
+            surface_water: character_planet_data['surface_water'],
+            terrain: character_planet_data['terrain'],
+            url: character_planet_data_url
+          )
+        else
+          character_planet = find_planet[0]
+        end
+      else
+        character_planet = Planet.where(name: 'Unknown')[0]
+      end
+
+
+      # retrieve the CHARACTERS RACE from the API and match it to the DB
+      if api_characters['species'].length != 0
+
+        character_race_planet = Planet.new
+        character_race = Race.new
+
+        character_race_url = replace_to_htmls(api_characters['species'][0])
+        character_race_data = get_data_url(character_race_url)
+        character_race_data_url = replace_to_htmls(character_race_data['url'])
+
+        if !character_race_data['homeworld'].present?
+          character_race_planet = Planet.where(name: 'Unknown')[0]
+        else
+          character_race_planet_api = get_data_url(replace_to_htmls(character_race_data['homeworld']))
+          if character_race_planet_api['homeworld'].present?
+            new_race_planet_url = replace_to_htmls(character_race_planet_api['homeworld'])
+            new_race_planet_data = get_data_url(new_race_planet_url)
+            new_race_planet_data_url = replace_to_htmls(new_race_planet_data['url'])
+            find_race_planet = Planet.where(name:  new_race_planet_data['name'])
+            if find_race_planet.length == 0
+              character_race_planet = Planet.find_or_create_by(
+                name: new_race_planet_data['name'],
+                climate: new_race_planet_data['climate'],
+                diameter: new_race_planet_data['diameter'],
+                gravity: new_race_planet_data['gravity'],
+                orbital_period: new_race_planet_data['orbital_period'],
+                population: new_race_planet_data['population'],
+                rotation_period: new_race_planet_data['rotation_period'],
+                surface_water: new_race_planet_data['surface_water'],
+                terrain: new_race_planet_data['terrain'],
+                url: new_race_planet_data_url
+              )
+            else
+              character_race_planet = find_race_planet[0]
+            end
+          else
+            character_race_planet = Planet.where(name: 'Unknown')[0]
+          end
+        end
+
+        find_race = Race.where(name: character_race_data['name'])
+        if find_race.length == 0
+          character_race = Race.find_or_create_by(
+            name: character_race_data['name'],
+            average_lifespan: character_race_data['average_lifespan'],
+            classification: character_race_data['classification'],
+            designation: character_race_data['designation'],
+            language: character_race_data['language'],
+            url: character_race_data_url,
+            planet: character_race_planet
+          )
+        else
+          character_race = find_race[0]
+        end
+      else
+        character_race = Race.where(name: 'Unknown')[0]
+      end
+
+      character_create = film_created.characters.find_or_create_by(
+              name: api_characters['name'],
+              birth_year: api_characters['birth_year'],
+              eye_color: api_characters['eye_color'],
+              gender: api_characters['gender'],
+              hair_color: api_characters['hair_color'],
+              height: api_characters['height'],
+              skin_color: api_characters['skin_color'],
+              url: api_characters_url,
+              planet: character_planet,
+              race: character_race
+            )
+      end
+    puts single_movie['title'] + ' DONE'
   end
 end
 
 # ------MAIN DATA RETRIEVAL-----
-main_data_retrieval()
-
+destroy_everything
+retrieve_films_and_planets
 
 # ------TESTS-----
 # first_film = Film.create(title: "Firt film")
@@ -100,36 +247,3 @@ main_data_retrieval()
 # puts new_char.errors.messages.inspect
 
 # puts new_planet.characters.first.name
-
-# first_film = Film.create(title: "Firt film")
-
-# puts "--FILMS--"
-# Film.all.each do |t|
-#   puts t.title
-# end
-
-# first_char = Character.create(name: 'Luke')
-# new_char = first_film.characters.create(name: 'Leia')
-
-# puts "--CHARACTERS--"
-# Character.all.each do |t|
-#   puts t.name
-# end
-
-# first_planet = Planet.create(name: "Jupiter")
-# new_planet = first_film.planets.create(name: "Saturn")
-
-# # puts new_planet.name
-# puts "--PLANETS--"
-# Planet.all.each do |t|
-#   puts t.name
-# end
-
-# first_specie = first_planet.species.create(name:"Human")
-
-# puts first_specie.errors.messages.inspect
-
-# puts "--SPECIE--"
-# Species.all.each do |t|
-#   puts t.name
-# end
